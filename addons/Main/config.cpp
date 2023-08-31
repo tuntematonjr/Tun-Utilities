95636ebd53f0918165ebda3c95635a0d9b227328
tuntematonjr <mika.lieto@hotmail.com>
2023-08-31 18:22:00 +0300

    Cleanup

--- addons/Main/config.cpp
+++ addons/Main/config.cpp
@@ -7,20 +7,8 @@ class CfgPatches
         units[] = { };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"cba_main","cba_xeh","cba_settings"};
+        requiredAddons[] = {"cba_xeh"};
         author = "Tuntematon";
         authorUrl = "https://github.com/tuntematonjr/Tun-Utilities";
     };
 };
-
-// class Extended_PostInit_EventHandlers {
-//     class Tun_Utilities_Main {
-//         init = QUOTE(call COMPILE_FILE(XEH_postInit));
-//     };
-// };
-
-// class Extended_PreInit_EventHandlers {
-//     class Tun_Utilities_Main {
-//         init = QUOTE( call COMPILE_FILE(XEH_preInit) );
-//     };
-// };
