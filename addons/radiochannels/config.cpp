12e33346e6ccbd5305bf7b44569b15cc427aa064
tuntematonjr <mika.lieto@hotmail.com>
2023-12-13 19:17:26 +0200

    add atribute to disable radiochannel creation

--- addons/radiochannels/config.cpp
+++ addons/radiochannels/config.cpp
@@ -17,3 +17,39 @@ class CfgPatches {
 
 // configs go here
 #include "CfgEventHandlers.hpp"
+
+class Cfg3DEN
+{
+    // Configuration of all objects
+    class Object
+    {
+        // Categories collapsible in "Edit Attributes" window
+        class AttributeCategories
+        {
+            // Category class, can be anything
+            class ADDON
+            {
+                displayName = "Tun Utilities - Radiochannels"; // Category name visible in Edit Attributes window
+                collapsed = 1; // When 1, the category is collapsed by default
+                class Attributes
+                {
+
+                    class GVAR(skipSquad)
+                    {
+                        displayName = "Skip radiochannel";
+                        tooltip = "When true, does not create any channels for this squad. Only group leaders setting has effect";
+                        property = QGVAR(skipSquadRadiochannel);
+                        control = "Checkbox";
+                        expression = QUOTE(if(leader group _this isEqualTo _this) then {(group _this) setVariable [ARR_3(QQGVAR(skipSquad),_value,true)];};);
+                        defaultValue = "false";
+                        unique = 0;
+                        condition = "objectBrain";
+                    };
+                };
+            };
+        };
+    };
+};
+
+
+
