8dcd094955e8fb2e522973fe09ac05be18600802
tuntematonjr <mika.lieto@hotmail.com>
2024-10-09 16:47:13 +0300

    Use ace moan fnc

--- addons/unconinfo/config.cpp
+++ addons/unconinfo/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {MAIN_ADDON_STR};
+        requiredAddons[] = {MAIN_ADDON_STR, "ace_medical_feedback"};
         author = "Tuntematon";
         authorUrl = GITHUBLINK;
         //VERSION_CONFIG;
