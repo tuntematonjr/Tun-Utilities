008d1300e768b776090c1640bf2b1b401f1eed75
tuntematonjr <mika.lieto@hotmail.com>
2025-06-18 11:23:35 +0300

    Update config.cpp

--- addons/serverinfo/config.cpp
+++ addons/serverinfo/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {"cba_main","cba_xeh","cba_settings","ace_modules",MAIN_ADDON_STR};
+		requiredAddons[] = {MAIN_ADDON_STR};
 		author = "Tuntematon";
 		authorUrl = GITHUBLINK;
 		VERSION_CONFIG;
